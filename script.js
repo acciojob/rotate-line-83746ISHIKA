959774e6b56a877cfda01aa6df18cccd0b9e3b5b
ISHIKA SOLANKI <ishikasolanki682@gmail.com>
2025-06-06 13:01:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const line = document.getElementById('line');
-let angle = 0;
+window.onload = function () {
+  const line = document.getElementById('line');
+  let angle = 0;
 
-setInterval(() => {
-  angle += 2;
-  line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
-}, 20);
+  setInterval(() => {
+    angle += 2;
+    line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+  }, 20);
+};
